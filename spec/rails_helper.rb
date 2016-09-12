9a4030dfe44bbaa35e54a25b349ef345d1fd4ebd
Deborah Hamel <deborahleehamel@gmail.com>
2016-09-12 09:20:58 -0600

    start feature test for nearest stations

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -19,7 +19,6 @@ end
 VCR.configure do |c|
   c.cassette_library_dir = "spec/casettes"
   c.hook_into :webmock
-  c.configure_rspec_metadata!
 end
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
